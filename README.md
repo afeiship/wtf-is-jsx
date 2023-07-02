ad337838bd6f868a8c4a7f6af9794c9126ef8646
afeiship <1290657123@qq.com>
2023-07-02 14:49:38 +0800

    docs: add start script

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-My work following the "WTF is JSX" tutorial. https://jasonformat.com/wtf-is-jsx/
+# wtf-is-jsx
+> My work following the "WTF is JSX" tutorial. https://jasonformat.com/wtf-is-jsx/
+
+## get-started
+```shell
+yarn 
+yarn build
+yarn start
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,30 @@
-var gulp = require('gulp');
-var babel = require('gulp-babel');
-var haml = require('gulp-haml');
-var del = require('del');
+var gulp = require("gulp");
+var babel = require("gulp-babel");
+var del = require("del");
 
 var paths = {
   scripts: {
-    src: 'src/js/**/*.js',
-    dest: 'build/js/'
+    src: "src/js/**/*.js",
+    dest: "build/js/",
   },
-  haml: {
-    src: 'src/**/*.haml',
-    dest: 'build/'
-  }
-}
+};
 
 function clean() {
-  return del([ 'build' ]);
+  return del(["build"]);
 }
 
 function scripts() {
-  return gulp.src(paths.scripts.src)
+  return gulp
+    .src(paths.scripts.src)
     .pipe(babel())
     .pipe(gulp.dest(paths.scripts.dest));
 }
 
 function pages() {
-  return gulp.src(paths.haml.src)
-    .pipe(haml())
-    .pipe(gulp.dest(paths.haml.dest));
+  return gulp.src("src/index.html").pipe(gulp.dest("build"));
 }
 
 var build = gulp.series(clean, gulp.parallel(scripts, pages));
 
-gulp.task('build', build);
-gulp.task('default', build);
+gulp.task("build", build);
+gulp.task("default", build);
